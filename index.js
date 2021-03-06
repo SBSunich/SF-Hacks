15076569792a869db1270b7faabd7e6a724c93fa
SBSpiderman <sunil.b.sunichura@gmail.com>
2021-03-06 02:27:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const axios = require("axios");
+
+
+const fun = async ()=>
+{
+  
+  const res = await axios.get("https://api.barcodelookup.com/v2/products?barcode=085239015759&key=7uym2paawshnadqi3jhk0d8mfm6f2h")
+  console.log(res.data.products[0].product_name)
+  var name = res.data.products[0].product_name;
+  
+
+const request = require('request');
+var query = name;
+request.get({
+  url: 'https://api.calorieninjas.com/v1/nutrition?query='+query,
+  headers: {
+    'X-Api-Key': 'VeOgjLOXI9psqqnIEoLlDw==MJtmYsJ2VyLtRLb1'
+  },
+}, function(error, response, body) {
+  if(error) return console.error('Request failed:', error);
+  else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+  else console.log(body)
+}); 
+}
+ 
+
+fun();
